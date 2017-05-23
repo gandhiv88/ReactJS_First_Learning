9fd8fbce60e04878dba3538553b17d7d05204a1f
Gandhi Valliappan <gandhiv88@gmail.com>
2017-05-22 21:39:05 -0400

    Tried event, props, state

--- src/app/Components/Home.js
+++ src/app/Components/Home.js
@@ -1,16 +1,61 @@
 import  React  from "react";
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
+
+    constructor(props){
+        super();
+        this.state ={
+            age : props.user.age,
+            msg : props.msg
+        };
+    }
+
+    makeMeOlder(){
+        this.setState({
+                age : this.state.age + 1
+        });
+    }
+
+    resetMyAge(){
+        this.setState({
+           age: this.props.user.age
+        });
+    }
+
+    onGreet(){
+        this.props.greet(this.props.user.name);
+    }
+
+    changeMessageValue(event){
+        this.props.msgChange(event.target.value);
+    }
     render(){
-        let content = "";
-        if(true){
-            content = <p>Mona's work is mokka work</p>;
-        }
         return(
             <div className="container">
-                <p> This is a new component</p>
-                {content}
+                <h4>User name is {this.props.user.name} and {(this.props.user.gender == "M") ? "His" : "Her"} age is {this.state.age}</h4>
+                <h2>{this.props.user.name}'s Hobbies are</h2>
+                <ul>
+                    {this.props.user.hobbies.map((hobby,i) => <li key={i}> {hobby}</li>)}
+                </ul>
+                {this.props.children}
+                <br/><br/>
+                <button onClick={() => {this.makeMeOlder()}} className="btn btn-primary">Make {this.props.user.name} older!</button>
+                <button onClick={() => {this.resetMyAge()}} className="btn btn-primary col-xs-offset-1">Reset Age</button>
+                <br/><br/>
+                <input value = {this.props.msg} type="text" onChange={(event) => {this.changeMessageValue(event)}}/>
+                <br/><br/>
+                <button onClick={() => {this.onGreet()}} className="btn btn-primary">Greet!!!</button>
             </div>
         );
     }
 }
+
+Home.protoTypes = {
+    user: PropTypes.object,
+    children : PropTypes.element.isRequired,
+    greet : PropTypes.func,
+    msgChange : PropTypes.func,
+    msg : PropTypes.string
+
+};
--- src/app/index.js
+++ src/app/index.js
@@ -5,17 +5,43 @@ import { Header } from "./Components/Header";
 import { Home } from "./Components/Home";
 
 class MyApp extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+          userName : "Welcome User",
+            message:"Type a message"
+        };
+    }
+    onGreet(userName){
+        this.setState({
+            userName: 'Welcome '+ userName
+        });
+    }
+
+    onMessageChange(value){
+        this.setState({
+            message:value
+        });
+    }
     render(){
+        var mona = {name:"Mona",age:24,hobbies : ["Tennis","Cooking","Painting"],gender:"F"};
+        var gandhi = {name:"Gandhi", age:29,hobbies:["Tennis","Cricket","Photography"],gender:"M"};
         return (
           <div className="container">
               <div className="row">
-                  <div className="col-xs-10 col-xs-offset-5">
-                      <Header/>
+                  <div className="col-xs-6 col-xs-offset-2">
+                      <Header homeLink="Home"/>
                   </div>
               </div>
               <div className="row">
-                  <div className="col-xs-10 col-xs-offset-5">
-                      <Home/>
+                  <div className="col-xs-6 col-xs-offset-2">
+                      <h1><u><b>{this.state.userName}</b></u></h1>
+                      <h2>{this.state.message}</h2>
+                      <Home user = {mona} greet = {this.onGreet.bind(this)} msg = {this.state.message} msgChange = {this.onMessageChange.bind(this)}/>
+                      <hr/>
+                      <Home user = {gandhi} greet = {this.onGreet.bind(this)} msg = {this.state.message} msgChange = {this.onMessageChange.bind(this)}>
+                      <p>Gandhi married Mona</p>
+                      </Home>
                   </div>
               </div>
           </div>
